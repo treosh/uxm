7ac8a26473c4b7d0e31c2950746a8b43fa2fab03
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2018-04-04 15:03:04 +0200

    remove experimental metrics

--- src/consistently-interactive.js
+++
@@ -1,7 +0,0 @@
-// naive implementation: find a last long task
-
-export function getConsistentlyInteractive({ firstContentfulPaint: fmp, domContentLoaded: dcl, longTasks }) {
-  const lastLongTask = longTasks[longTasks.length - 1]
-  const lastTaskEnd = lastLongTask ? lastLongTask.startTime + lastLongTask.duration : 0
-  return Math.max(lastTaskEnd, fmp, dcl)
-}
