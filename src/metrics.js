a6fdbb925e5c25974ecc7b6ffff5c5facb268c0a
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2019-11-19 15:12:56 +0100

    experiment with export createMetricObserver

--- src/metrics.js
+++ src/metrics.js
@@ -1,14 +1,15 @@
-import { getEventsByType } from './performance-observer'
-import { round } from './utils'
+import { getEventsByType, createEventsObserver } from './performance-observer'
+import { round, debug } from './utils'
 
 /** @typedef {'first-contentful-paint' | 'first-input-delay' | 'largest-contentful-paint' | 'cumulative-layout-shift'} StrictMetricType */
 /** @typedef {StrictMetricType | 'fcp' | 'lcp' | 'fid' | 'cls'} MetricType */
-/** @typedef {(metric: number | null) => any} PerformanceMetricCallback */
+/** @typedef {(metric: number | null) => any} MetricObserverCallback */
 
 const FCP = 'first-contentful-paint'
 const FID = 'first-input-delay'
 const LCP = 'largest-contentful-paint'
 const CLS = 'cumulative-layout-shift'
+const visibilityChange = 'visibilitychange'
 
 /**
  * Get metric by `metricType`.
@@ -20,56 +21,113 @@ const CLS = 'cumulative-layout-shift'
 
 export function getMetricByType(metricType) {
   const metric = normalizeMetricType(metricType)
-  switch (metric) {
+  const eventType = metric === FID ? 'first-input' : metric === CLS ? 'layout-shift' : metric
+  return getEventsByType(eventType).then(events => {
+    switch (metric) {
+      case FCP:
+        return computeFcp(events)
+      case LCP:
+        return computeLcp(events)
+      case FID:
+        return computeFid(events)
+      case CLS:
+        return computeCls(events)
+    }
+  })
+}
+
+/**
+ * Observe `metricType`, if the value is already observed, return it.
+ *
+ * @param {MetricType} metricType
+ * @param {MetricObserverCallback} callback
+ */
+
+export function createMetricObserver(metricType, callback) {
+  const opts = { buffered: true }
+  switch (normalizeMetricType(metricType)) {
     case FCP:
-      return getFcp()
+      const fcpObserver = createEventsObserver(
+        FCP,
+        paintEvents => {
+          const fcp = computeFcp(paintEvents)
+          if (fcp) {
+            fcpObserver.disconnect()
+            callback(fcp)
+          }
+        },
+        opts
+      )
+      break
     case FID:
-      return getFid()
+      const fidObserver = createEventsObserver(
+        'first-input',
+        fidEvents => {
+          fidObserver.disconnect()
+          callback(computeFid(fidEvents))
+        },
+        opts
+      )
+      break
     case LCP:
-      return getLcp()
+      let lcp = 0
+      let lcpVisibilityChangeListener = () => {
+        debug('%s %s', visibilityChange, LCP)
+        lcpObserver.disconnect()
+        document.removeEventListener(visibilityChange, lcpVisibilityChangeListener, true)
+        callback(lcp)
+      }
+      let lcpObserver = createEventsObserver(LCP, lcpEvents => (lcp = computeLcp(lcpEvents)), opts)
+      document.addEventListener(visibilityChange, lcpVisibilityChangeListener, true)
+      break
     case CLS:
-      return getCls()
-    default:
-      throw new Error(`Invalid metric: ${metricType}`)
+      let cls = 0
+      let clsVisibilityChangeListener = () => {
+        debug('%s %s', visibilityChange, CLS)
+        clsObserver.takeRecords()
+        clsObserver.disconnect()
+        document.removeEventListener(visibilityChange, clsVisibilityChangeListener, true)
+        callback(cls)
+      }
+      let clsObserver = createEventsObserver('layout-shift', lsEvents => (cls += computeCls(lsEvents)), opts)
+      document.addEventListener(visibilityChange, clsVisibilityChangeListener, true)
+      break
   }
 }
 
-function getFcp() {
-  return getEventsByType(FCP).then(paintEvents => {
-    const fcpEvent = paintEvents.find(e => e.name === FCP)
-    return fcpEvent ? round(fcpEvent.startTime) : null
-  })
+/** @param {PerformanceEntry[]} paintEvents */
+function computeFcp(paintEvents) {
+  const [fcpEvent] = paintEvents.filter(e => e.name === FCP)
+  return fcpEvent ? round(fcpEvent.startTime) : null
 }
 
-function getLcp() {
-  return getEventsByType(LCP).then(lcpEvents => {
-    if (!lcpEvents.length) return null
-    const lastLcpEvent = lcpEvents[lcpEvents.length - 1]
-    return round(lastLcpEvent.renderTime || lastLcpEvent.loadTime)
-  })
+/** @param {PerformanceEntry[]} lcpEvents */
+function computeLcp(lcpEvents) {
+  const lastLcpEvent = lcpEvents[lcpEvents.length - 1]
+  return round(lastLcpEvent.renderTime || lastLcpEvent.loadTime)
 }
 
-function getFid() {
-  return getEventsByType('first-input').then(([fidEvent]) => {
-    return fidEvent ? round(fidEvent.processingStart - fidEvent.startTime) : null
-  })
+/** @param {PerformanceEntry[]} fidEvents */
+function computeFid([fidEvent]) {
+  return fidEvent ? round(fidEvent.processingStart - fidEvent.startTime) : null
 }
 
-function getCls() {
-  return getEventsByType('layout-shift').then(lsEvents => {
-    if (!lsEvents.length) return null
-    const cls = lsEvents.reduce((memo, lsEvent) => {
-      // Only count layout shifts without recent user input.
-      // and collect percentage value
-      if (!lsEvent.hadRecentInput) memo += 100 * lsEvent.value
-      return memo
-    }, 0)
-    return round(cls, 3)
-  })
+/** @param {PerformanceEntry[]} lsEvents */
+function computeCls(lsEvents) {
+  const cls = lsEvents.reduce((memo, lsEvent) => {
+    // Only count layout shifts without recent user input.
+    // and collect percentage value
+    if (!lsEvent.hadRecentInput) memo += 100 * lsEvent.value
+    return memo
+  }, 0)
+  return round(cls, 3)
 }
 
-/** @param {MetricType} metricType @return {string} */
+/** @param {MetricType} metricType @return {StrictMetricType} */
 function normalizeMetricType(metricType) {
-  const metric = metricType.toLowerCase()
-  return metric === 'fcp' ? FCP : metric === 'fid' ? FID : metric === 'lcp' ? LCP : metric === 'cls' ? CLS : metric
+  const m = metricType.toLowerCase()
+  const metric = m === 'fcp' ? FCP : m === 'fid' ? FID : m === 'lcp' ? LCP : m === 'cls' ? CLS : m
+  if ([FCP, FID, LCP, CLS].indexOf(metric) === -1) throw new Error(`Invalid metric: ${metricType}`)
+  // @ts-ignore
+  return metric
 }
